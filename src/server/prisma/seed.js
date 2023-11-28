aefff9b031fac7e61bae134c8aaeb9a0feab9647
RafaelLSuarez1997 <63333102+RafaelLSuarez1997@users.noreply.github.com>
2023-11-28 11:36:19 -0500

    test 123

--- src/server/prisma/seed.js
+++ src/server/prisma/seed.js
@@ -1,15 +1,66 @@
 const prisma = require("../prisma");
 const mockdata = [
   {
-    brand: "Nike",
-    category: "Mens shoes",
-    size: 7.5,
+    "brand": "Nike",
+    "category": "Mens shoes",
+    "size": 7.5
   },
   {
-    brand: "Adidas",
-    category: "Women shoes",
-    size: 6,
+    "brand": "Adidas",
+    "category": "Women shoes",
+    "size": 6
   },
+  {
+    "brand": "Puma",
+    "category": "Mens shoes",
+    "size": 9
+  },
+  {
+    "brand": "Reebok",
+    "category": "Women shoes",
+    "size": 8.5
+  },
+  {
+    "brand": "New Balance",
+    "category": "Mens shoes",
+    "size": 10
+  },
+  {
+    "brand": "Under Armour",
+    "category": "Women shoes",
+    "size": 7
+  },
+  {
+    "brand": "ASICS",
+    "category": "Mens shoes",
+    "size": 8
+  },
+  {
+    "brand": "Skechers",
+    "category": "Women shoes",
+    "size": 9.5
+  },
+  {
+    "brand": "Vans",
+    "category": "Mens shoes",
+    "size": 11
+  },
+  {
+    "brand": "Converse",
+    "category": "Women shoes",
+    "size": 7.5
+  },
+  {
+    "brand": "Brooks",
+    "category": "Mens shoes",
+    "size": 9.5
+  },
+  {
+    "brand": "Fila",
+    "category": "Women shoes",
+    "size": 8
+  }
+  
 ]
 const seed = async () => {
 for (item of mockdata) {
