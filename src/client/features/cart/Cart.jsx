7c53334c5d59074ad98b9000d60d61bf44deb6bb
Ralph Fang <ralphrainierfang@yahoo.com>
2023-11-30 15:14:07 -0500

    Fixed the Cart issue with removing item from cart

--- src/client/features/cart/Cart.jsx
+++ src/client/features/cart/Cart.jsx
@@ -2,47 +2,51 @@ import React, { useContext } from 'react';
 import { ShopContext } from './ShopContext';
 import { useGetItemQuery, useDeleteItemMutation } from '../items/itemSlice';
 
+function CartItem({ itemId, quantity, size }) {
+  const { removeFromCart } = useContext(ShopContext);
+  const { data: item, isLoading } = useGetItemQuery(itemId);
+
+  if (isLoading) {
+    return <p>Loading . . .</p>;
+  }
+
+  if (!item) {
+    return <p>Item not found</p>;
+  }
+
+  const onDelete = () => {
+    removeFromCart(itemId);
+  };
+
+  return (
+    <li key={itemId}>
+      <img
+        className="item-cart"
+        src={item.imageUrl}
+        alt={item.brand}
+        style={{ width: '100px', height: '100px' }}
+      />
+      <div>
+        <p>Item ID: {itemId}</p>
+        <p>Brand: {item.brand}</p>
+        <p>Size: {size}</p>
+        <p>Quantity: {quantity}</p>
+        <button onClick={onDelete}>Remove from Cart</button>
+      </div>
+    </li>
+  );
+}
 
 function Cart() {
-  const { cartItems, removeFromCart } = useContext(ShopContext);
-  
-  const onDelete = async (itemId) => {
-   removeFromCart(itemId)
-  }
+  const { cartItems } = useContext(ShopContext);
 
   return (
     <div>
       <h1>Shopping Cart</h1>
       <ul>
-        {Object.entries(cartItems).map(([itemId, { quantity, size }]) => {
-          const { data: item, isLoading } = useGetItemQuery(itemId);
-
-          if (isLoading) {
-            return <p>Loading . . .</p>;
-          }
-
-          if (!item) {
-            return <p>Item not found</p>;
-          }
-
-          return (
-            <li key={itemId}>
-              <img
-                className="item-cart"
-                src={item.imageUrl}
-                alt={item.brand}
-                style={{ width: '100px', height: '100px' }}
-              />
-              <div>
-                <p>Item ID: {itemId}</p>
-                <p>Brand: {item.brand}</p>
-                <p>Size: {size}</p>
-                <p>Quantity: {quantity}</p>
-                <button onClick={() => onDelete(itemId)}>Remove from Cart</button>
-              </div>
-            </li>
-          );
-        })}
+        {Object.entries(cartItems).map(([itemId, { quantity, size }]) => (
+          <CartItem key={itemId} itemId={itemId} quantity={quantity} size={size} />
+        ))}
       </ul>
     </div>
   );
