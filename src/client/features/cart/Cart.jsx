0b2785b6702867350330b9c923818a29424773d2
zhiarTX <138315070+zhiarTX@users.noreply.github.com>
2023-11-30 14:08:46 -0700

    Update Cart.jsx
    To include the Continue shopping button and Total Price

--- src/client/features/cart/Cart.jsx
+++ src/client/features/cart/Cart.jsx
@@ -1,51 +1,78 @@
 import React, { useContext } from 'react';
 import { ShopContext } from './ShopContext';
 import { useGetItemQuery, useDeleteItemMutation } from '../items/itemSlice';
+import { Link } from 'react-router-dom';
+import './Cart.css';
 
 
-function Cart() {
-  const { cartItems, removeFromCart } = useContext(ShopContext);
-  
-  const onDelete = async (itemId) => {
-   removeFromCart(itemId)
+
+function CartItem({ itemId, quantity, size }) {
+  const { removeFromCart } = useContext(ShopContext);
+  const { data: item, isLoading } = useGetItemQuery(itemId);
+  if (isLoading) {
+    return <p>Loading . . .</p>;
   }
+  if (!item) {
+    return <p>Item not found</p>;
+  }
+  const onDelete = () => {
+    removeFromCart(itemId);
+  };
+
+
+
+  return (
+    <li key={itemId}>
+      <img
+        className="item-cart"
+        src={item.imageUrl}
+        alt={item.brand}
+        style={{ width: '100px', height: '100px' }}
+      />
+      <div>
+        <p>Item ID: {itemId}</p>
+        <p>Brand: {item.brand}</p>
+        <p>Size: {size}</p>
+        <p>Quantity: {quantity}</p>
+        <p>Price: ${item.price}</p>
+        <br/>
+        <button onClick={onDelete}>Remove from Cart</button>
+      </div>
+    </li>
+  );
+}
+function Cart() {
+  const { cartItems } = useContext(ShopContext);
+  const calculateTotalAmount = () => {
+    let totalAmount = 0;
+  
+    Object.entries(cartItems).forEach(([itemId, { quantity }]) => {
+      const { data: item, isLoading } = useGetItemQuery(itemId);
+  
+      if (!isLoading && item) {
+        totalAmount += item.price * quantity;
+      }
+    });
+    return totalAmount;
+  }
+
 
   return (
     <div>
       <h1>Shopping Cart</h1>
       <ul>
-        {Object.entries(cartItems).map(([itemId, { quantity, size }]) => {
-          const { data: item, isLoading } = useGetItemQuery(itemId);
-
-          if (isLoading) {
-            return <p>Loading . . .</p>;
-          }
-
-          if (!item) {
-            return <p>Item not found</p>;
-          }
-
-          return (
-            <li key={itemId}>
-              <img
-                className="item-cart"
-                src={item.imageUrl}
-                alt={item.brand}
-                style={{ width: '100px', height: '100px' }}
-              />
-              <div>
-                <p>Item ID: {itemId}</p>
-                <p>Brand: {item.brand}</p>
-                <p>Size: {size}</p>
-                <p>Quantity: {quantity}</p>
-                <button onClick={() => onDelete(itemId)}>Remove from Cart</button>
-              </div>
-            </li>
-          );
-        })}
+        {Object.entries(cartItems).map(([itemId, { quantity, size }]) => (
+          <CartItem key={itemId} itemId={itemId} quantity={quantity} size={size} />
+        ))}
       </ul>
+      <br/>
+      <p>Total Amount: ${calculateTotalAmount()} </p>
+      <br/>
+      <Link to ="/">
+        <button>Continue Shopping</button>
+      </Link>
+      
     </div>
   );
 }
-
 export default Cart;
