16f065ede2e156bca20f82da5928ba034e393b90
mvail9001@gmail.com <mvail9001@gmail.com  git config --global user.name  Your Name>
2023-12-04 15:49:52 -0500

    Added front end auth priv in singleitem

--- src/client/features/items/SingleItem.jsx
+++ src/client/features/items/SingleItem.jsx
@@ -2,9 +2,9 @@ import React, { useState, useContext, useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
 import { useGetItemQuery, useGetItemsQuery } from "./itemSlice"; 
 import "./Singleitem.less";
-
 import { ShopContext } from "../cart/ShopContext";
-
+import { selectToken } from "../auth/authSlice";
+import { useSelector } from "react-redux";
 
 export default function SingleItem() {
   const { id } = useParams();
@@ -13,6 +13,7 @@ export default function SingleItem() {
   const [selectedSize, setSelectedSize] = useState(0);
   const { addToCart } = useContext(ShopContext);
   const [recommendedItems, setRecommendedItems] = useState([]);
+  const authToken = useSelector(selectToken); 
 
   useEffect(() => {
     if (!isItemLoading && !isAllItemsLoading && currentItem && allItems) {
@@ -42,44 +43,47 @@ export default function SingleItem() {
   if (!currentItem) {
     return <p>Item not found</p>;
   }
-
-  return (
-    <div>
-      <div className="single-item-container">
-        <img src={currentItem.imageUrl} alt={currentItem.brand} className="item-image" />
-        <div className="item-details">
-          <p className="item-brand">{currentItem.brand}</p>
-          <p className="item-category">{currentItem.category}</p>
-          <p className="item-size">${currentItem.price}</p>
-          <label className="size-range">Select Size:</label>
-          <input
-            type="range"
-            id="size"
-            name="size"
-            min="0"
-            max="20"
-            step="0.5"
-            value={selectedSize}
-            onChange={handleSizeChange}
-          />
-          <p>Selected Size: {selectedSize}</p>
-          <p>Description: {currentItem.description}</p>
-          <button className="addtocartbutton" onClick={handleAddToCart}>
-            Add to Cart
-          </button>
+  if (authToken) {
+    return (
+      <div>
+        <div className="single-item-container">
+          <img src={currentItem.imageUrl} alt={currentItem.brand} className="item-image" />
+          <div className="item-details">
+            <p className="item-brand">{currentItem.brand}</p>
+            <p className="item-category">{currentItem.category}</p>
+            <p className="item-size">${currentItem.price}</p>
+            <label className="size-range">Select Size:</label>
+            <input
+              type="range"
+              id="size"
+              name="size"
+              min="0"
+              max="20"
+              step="0.5"
+              value={selectedSize}
+              onChange={handleSizeChange}
+            />
+            <p>Selected Size: {selectedSize}</p>
+            <p>Description: {currentItem.description}</p>
+            <button className="addtocartbutton" onClick={handleAddToCart}>
+              Add to Cart
+            </button>
+          </div>
+        </div>
+        <div className="recommended-items">
+          <h3>Recommended Items:</h3>
+          {recommendedItems.map((item) => (
+            <Link key={item.id} to={`/${item.id}`} className="recommended-item">
+              <img src={item.imageUrl} alt={item.brand} />
+              <p>{item.brand}</p>
+              <p>${item.price}</p>
+            </Link>
+          ))}
         </div>
       </div>
-      <div className="recommended-items">
-        <h3>Recommended Items:</h3>
-        {recommendedItems.map(item => (
-          <Link key={item.id} to={`/${item.id}`} className="recommended-item">
-            <img src={item.imageUrl} alt={item.brand} />
-            <p>{item.brand}</p>
-            <p>${item.price}</p>
-          </Link>
-        ))}
-      </div>
-    </div>
-  );
-
+    );
+  } else {
+    // You can redirect the user to the login page or display a message
+    return <p>Please log in to view this item.</p>;
+  }
 }
