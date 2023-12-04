23be75ce2b7e07b744569ca4240a10601c870cfb
RafaelLSuarez1997 <63333102+RafaelLSuarez1997@users.noreply.github.com>
2023-12-04 10:58:22 -0500

    pull

--- src/client/features/items/SpecificItems.jsx
+++ src/client/features/items/SpecificItems.jsx
@@ -1,28 +1,28 @@
+import { useGetItemsQuery } from "./itemSlice";
 import React from "react";
-import { useParams } from "react-router-dom";
-import { useGetItemQuery } from "./itemSlice";
-import "./SpecificItems.less"; 
+import { Link } from "react-router-dom";
+import "./items.less";
 
-export default function SpecificItems() {
-  const { id } = useParams();
-  const { data: item, isLoading } = useGetItemQuery(id);
 
-  if (isLoading) {
-    return <p>Loading...</p>;
-  }
-
-  if (!item) {
-    return <p>Item not found</p>;
-  }
-
-  return (
-    <div className="specific-item-container">
-      <img src={item.imageUrl} className="specific-item-image" />
-      <div className="specific-item-details">
-        <p className="specific-item-brand">{item.brand}</p>
-        <p className="specific-item-category">{item.category}</p>
-        <p className="specific-item-price">{item.price}</p> 
+export default function Items() {
+    const { data: items, isLoading } = useGetItemsQuery();
+    console.log(items);
+    if (isLoading) {
+      return <p>is loading</p>;
+    }
+  
+    return (
+      <div className="items-container">
+        {items.map((item) => (
+          <Link to={`/${item.id}`} key={item.id} className="item-card">
+            <img src={item.imageUrl} alt={item.brand} className="item-image" />
+            <div className="item-details">
+              <p className="item-brand">{item.brand}</p>
+              <p className="item-category">{item.category}</p>
+              <p className="item-size">{item.price}</p>
+            </div>
+          </Link>
+        ))}
       </div>
-    </div>
-  );
-}
+    );
+  }
