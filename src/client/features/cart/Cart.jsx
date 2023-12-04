1fffab50bcca5e08195af1a25f47ee220df6dac2
zhiarTX <138315070+zhiarTX@users.noreply.github.com>
2023-12-04 06:06:05 -0700

    fixed decimal place

--- src/client/features/cart/Cart.jsx
+++ src/client/features/cart/Cart.jsx
@@ -4,10 +4,12 @@ import { ShopContext } from './ShopContext';
 import { useGetItemQuery } from '../items/itemSlice';
 import { Link } from 'react-router-dom';
 import './Cart.css';
+import { current } from 'immer';
 
 function CartItem({ itemId, quantity, size }) {
   const { removeFromCart } = useContext(ShopContext);
   const { data: item, isLoading } = useGetItemQuery(itemId);
+  const [currentQuantity, setCurrentQuantity] = useState(quantity);
 
   if (isLoading) {
     return <p>Loading . . .</p>;
@@ -21,6 +23,20 @@ function CartItem({ itemId, quantity, size }) {
     removeFromCart(itemId);
   };
 
+  const increaseQuantity = ()=> {
+    if (currentQuantity < 10){
+      setCurrentQuantity (currentQuantity + 1);
+    }
+  };
+
+  const decreaseQuantity =()=> {
+    if (currentQuantity > 1) {
+      setCurrentQuantity (currentQuantity - 1);
+    }
+  };
+
+  const totalPrice = item.price * currentQuantity;
+
   return (
     <li key={itemId}>
       <img
@@ -33,8 +49,12 @@ function CartItem({ itemId, quantity, size }) {
         <p>Item ID: {itemId}</p>
         <p>Brand: {item.brand}</p>
         <p>Size: {size}</p>
-        <p>Quantity: {quantity}</p>
-        <p>Price: ${item.price}</p>
+        <div>
+          <button onClick={decreaseQuantity}>-</button>
+          <span style = {{ margin: '0 10 px'}}>{currentQuantity}</span>
+          <button onClick={increaseQuantity}>+</button>
+        </div>
+        <p>Price: ${totalPrice.toFixed(2)}</p>
         <br/>
         <button onClick={onDelete}>Remove from Cart</button>
       </div>
@@ -50,23 +70,30 @@ function Cart() {
     const calculateTotalAmount = async () => {
       let calculatedTotalAmount = 0;
 
-      for (const [itemId, { quantity }] of Object.entries(cartItems)) {
-        try {
-          // Fetch item details using axios DOWNLOAD AXIOS
-          const response = await axios.get(`/api/items/${itemId}`);
-          const item = response.data;
+      const itemIds = Object.keys(cartItems);
+      const itemDetailsPromises = itemIds.map((itemId) => axios.get(`/api/items/${itemId}`));
 
+      try {
+        const itemDetailsResponses = await Promise.all(itemDetailsPromises);
+        const itemDetails = itemDetailsResponses.map((response) => response.data);
+
+        for (const item of itemDetails) {
+          const itemId = item.id;
+          const quantity = cartItems[itemId].quantity;
           calculatedTotalAmount += item.price * quantity;
-        } catch (error) {
-          console.error(`Error fetching item details for itemId ${itemId}:`, error);
         }
-      }
 
-      setTotalPrice(calculatedTotalAmount);
+        setTotalPrice(calculatedTotalAmount.toFixed(2));
+      } catch (error) {
+        console.error('Error fetching item details:', error);
+      }
     };
 
     calculateTotalAmount();
   }, [cartItems]);
+
+
+  
 
   return (
     <div>
