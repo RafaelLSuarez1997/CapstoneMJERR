930c079043a8ef28e843a72fa2a82f68057a9586
zhiarTX <138315070+zhiarTX@users.noreply.github.com>
2023-12-07 14:43:59 -0700

    updated cart.jsx with previous code

--- src/client/features/cart/Cart.jsx
+++ src/client/features/cart/Cart.jsx
@@ -1,20 +1,50 @@
 import React, { useContext, useEffect, useState } from 'react';
-import axios from 'axios';  // Import axios
+import axios from 'axios';
 import { ShopContext } from './ShopContext';
 import { useGetItemQuery } from '../items/itemSlice';
 import { Link } from 'react-router-dom';
-function CartItem({ itemId, quantity, size }) {
+import './Cart.less';
+import { selectToken } from '../auth/authSlice';
+import { useSelector } from 'react-redux';
+
+function CartItem({ itemId, quantity, size, updateTotalPrice }) {
   const { removeFromCart } = useContext(ShopContext);
   const { data: item, isLoading } = useGetItemQuery(itemId);
+  const [currentQuantity, setCurrentQuantity] = useState(quantity);
+  const [totalItemQuantity, setTotalItemQuantity] = useState(quantity);
+
+  useEffect(() => {
+    setTotalItemQuantity(quantity);
+  }, [quantity]);
+
   if (isLoading) {
     return <p>Loading . . .</p>;
   }
+
   if (!item) {
     return <p>Item not found</p>;
   }
+
   const onDelete = () => {
     removeFromCart(itemId);
   };
+
+  const increaseQuantity = () => {
+    if (currentQuantity < 10) {
+      setCurrentQuantity(currentQuantity + 1);
+      setTotalItemQuantity(totalItemQuantity + 1);
+      updateTotalPrice(item.price, 1);
+    }
+  };
+
+  const decreaseQuantity = () => {
+    if (currentQuantity > 1) {
+      setCurrentQuantity(currentQuantity - 1);
+      setTotalItemQuantity(totalItemQuantity - 1);
+      updateTotalPrice(item.price, -1)
+    }
+  };
+
   return (
     <li key={itemId}>
       <img
@@ -27,24 +57,34 @@ function CartItem({ itemId, quantity, size }) {
         <p>Item ID: {itemId}</p>
         <p>Brand: {item.brand}</p>
         <p>Size: {size}</p>
-        <p>Quantity: {quantity}</p>
-        <p>Price: ${item.price}</p>
-        <br/>
+        <div>
+          <button onClick={decreaseQuantity}>-</button>
+          <span style={{ margin: '0 10px' }}>{currentQuantity}</span>
+          <button onClick={increaseQuantity}>+</button>
+        </div>
+        <br />
         <button onClick={onDelete}>Remove from Cart</button>
       </div>
     </li>
   );
 }
+
 function Cart() {
   const { cartItems } = useContext(ShopContext);
   const [totalPrice, setTotalPrice] = useState(0);
+  const authToken = useSelector(selectToken);
+
+  const updateTotalPrice = (itemPrice, quantityChange) => {
+    setTotalPrice((prevTotalPrice) => prevTotalPrice + itemPrice * quantityChange);
+  };
+
   useEffect(() => {
     const calculateTotalAmount = async () => {
       let calculatedTotalAmount = 0;
       for (const [itemId, { quantity }] of Object.entries(cartItems)) {
         try {
           // Fetch item details using axios DOWNLOAD AXIOS
-          const response = await axios.get(`/api/items/${itemId}`);
+          const response = await axios.get(`/api/items/id/${itemId}`);
           const item = response.data;
           calculatedTotalAmount += item.price * quantity;
         } catch (error) {
@@ -55,24 +95,30 @@ function Cart() {
     };
     calculateTotalAmount();
   }, [cartItems]);
-  return (
-    <div>
-      <h1>Shopping Cart</h1>
-      <ul>
-        {Object.entries(cartItems).map(([itemId, { quantity, size }]) => (
-          <CartItem key={itemId} itemId={itemId} quantity={quantity} size={size} />
-        ))}
-      </ul>
-      <br/>
-      <p>Total Amount: ${totalPrice.toFixed(2)} </p>
-      <br/>
-      <Link to ="/">
-        <button>Continue Shopping</button>
-      </Link>
-      <Link to ="/checkout">
-        <button>Proceed to Checkout</button>
-      </Link>
-    </div>
-  );
+
+  if (authToken) {
+    return (
+      <div>
+        <h1>Shopping Cart</h1>
+        <ul>
+          {Object.entries(cartItems).map(([itemId, { quantity, size }]) => (
+            <CartItem key={itemId} itemId={itemId} quantity={quantity} size={size} updateTotalPrice={updateTotalPrice} />
+          ))}
+        </ul>
+        <br />
+        <p>Total Amount: ${totalPrice.toFixed(2)} </p>
+        <br />
+        <Link to="/">
+          <button>Continue Shopping</button>
+        </Link>
+        <Link to="/checkout">
+          <button>Proceed to Checkout</button>
+        </Link>
+      </div>
+    );
+  } else {
+    return <p>Please log in or sign up to view your cart</p>;
+  }
 }
+
 export default Cart;
